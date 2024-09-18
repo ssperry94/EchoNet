40d497c6b571de04dbd3061a6e1cf9d07b832671
Sam Perry <s.sperry94@gmail.com>
2024-09-18 09:41:42 -0500

    BRANCH:main................added a simple hello world program to make sure all members can compile and run java programs.

---
+++ test.java
@@ -0,0 +1,10 @@
+/*
+ * A test Hello World program to make sure all of our hardware is compatible
+ */
+
+
+public class test {
+    public static void main (String [] args) {
+        System.out.println("Hello, World!");
+    }
+}
