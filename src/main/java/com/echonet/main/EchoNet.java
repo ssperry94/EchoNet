4bc6eba41541930ce05588372bdc81b36ea48019
Sam Perry <s.sperry94@gmail.com>
2024-09-22 15:52:42 -0500

    BRANCH:main.........added the pom.xml, .gitignore, and src folder generated from NetBeans.

---
+++ src/main/java/com/echonet/main/EchoNet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.echonet.main;
+
+/**
+ *
+ * @author sperry94
+ */
+public class EchoNet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
