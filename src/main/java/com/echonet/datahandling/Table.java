4a0e1e9c5094a474f4a35c83dc11a49336311ee3
Sam Perry <s.sperry94@gmail.com>
2024-10-24 15:26:27 -0500

    BRANCH:Table...........created a class representing a table from the database.

---
+++ src/main/java/com/echonet/datahandling/Table.java
@@ -0,0 +1,12 @@
+package com.echonet.datahandling;
+
+import java.util.List;
+
+public class Table {
+    private String tableName;
+    private List <String> columnNames;
+
+    public Table(String tableName) {
+        
+    }
+}
