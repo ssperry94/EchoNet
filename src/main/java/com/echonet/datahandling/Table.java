7e84f60c8805994129bdd200d793d6bbcb2c193c
Sam Perry <s.sperry94@gmail.com>
2024-10-25 10:43:39 -0500

    BRANCH:Table.........created a method to update the column names if needed.

--- src/main/java/com/echonet/datahandling/Table.java
+++ src/main/java/com/echonet/datahandling/Table.java
@@ -8,13 +8,15 @@ import com.echonet.exceptions.DataBaseNotFoundException;
 public class Table {
     private String tableName;
     private List <String> columnNames;
+    private DataHanlder handler;
 
     public Table(final String tableName) throws SQLException, ClassNotFoundException, DataBaseNotFoundException {
         this.tableName = tableName;
-        DataHanlder handler = new DataHanlder();
-        this.columnNames = handler.getTableColumnNames(tableName);
+        this.handler = new DataHanlder();
+        this.columnNames = this.handler.getTableColumnNames(tableName);
     }
 
     public List <String> getTableColumns() {return this.columnNames;}
     public String getTableName() {return this.tableName;}
+    public void updateColumnNames() throws SQLException {this.columnNames = this.handler.getTableColumnNames(this.tableName);}
 }
