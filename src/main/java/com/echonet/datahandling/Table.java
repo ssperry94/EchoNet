981c4934ea3344b5f3058c5320a717df35055248
Sam Perry <s.sperry94@gmail.com>
2024-10-25 10:39:32 -0500

    BRANCH:table...........created the constructor and getters for the Table class.

--- src/main/java/com/echonet/datahandling/Table.java
+++ src/main/java/com/echonet/datahandling/Table.java
@@ -1,12 +1,20 @@
 package com.echonet.datahandling;
 
+import java.sql.SQLException;
 import java.util.List;
+
+import com.echonet.exceptions.DataBaseNotFoundException;
 
 public class Table {
     private String tableName;
     private List <String> columnNames;
 
-    public Table(String tableName) {
-        
+    public Table(final String tableName) throws SQLException, ClassNotFoundException, DataBaseNotFoundException {
+        this.tableName = tableName;
+        DataHanlder handler = new DataHanlder();
+        this.columnNames = handler.getTableColumnNames(tableName);
     }
+
+    public List <String> getTableColumns() {return this.columnNames;}
+    public String getTableName() {return this.tableName;}
 }
