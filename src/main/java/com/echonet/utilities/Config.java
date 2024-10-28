e65d2eea65534201a1dfb1a19e828002d87b36f6
Sam Perry <s.sperry94@gmail.com>
2024-10-28 10:04:00 -0500

    BRANCH:Config........created a class to hold all constant values.

---
+++ src/main/java/com/echonet/utilities/Config.java
@@ -0,0 +1,10 @@
+package com.echonet.utilities;
+
+/**
+ * A class containing all public static final variables that represent constants
+ */
+public class Config {
+    public static final String DATABASE_INIT = "jdbc:sqlite:echodata.db";
+    public static final String DATABASE_PATH = "echodata.db";
+    public static final String DATABASE_DRIVER = "org.sqlite.JDBC";
+}
