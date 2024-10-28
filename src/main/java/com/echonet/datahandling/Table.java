8c275f75d4a47c967ccc4e82e1519dda1b30786e
Sam Perry <s.sperry94@gmail.com>
2024-10-28 11:01:00 -0500

    BRANCH:Config.........added javadocs for the table class.

--- src/main/java/com/echonet/datahandling/Table.java
+++ src/main/java/com/echonet/datahandling/Table.java
@@ -6,28 +6,52 @@ import java.util.List;
 import com.echonet.exceptions.DataBaseNotFoundException;
 import com.echonet.utilities.Config;
 
+/**
+ * A class representing a table in the database
+ */
 public class Table {
     private String tableName;
     private List <String> columnNames;
     private DataHanlder handler;
 
+    /**
+     * Initalizes the table object. Will querey the databse for this table, and return an SQLException if the table isn't found
+     * @param tableName - name of the table to look for
+     * @throws SQLException - thrown either during initalization of the DataHanlder object, or if the table cannot be found.
+     * @throws ClassNotFoundException - thrown if the driver JDBC uses cannot be accessed
+     * @throws DataBaseNotFoundException - thrown if the database connection cannot be established 
+     */
     public Table(final String tableName) throws SQLException, ClassNotFoundException, DataBaseNotFoundException {
         this.tableName = tableName;
         this.handler = new DataHanlder(Config.DATABASE_INIT);
         this.columnNames = this.handler.getTableColumnNames(tableName);
     }
 
+    /**
+     * Getter to return the column names for the table
+     * @return the column names belonging to the table
+     */
     public List <String> getTableColumns() {return this.columnNames;}
+
+    /**
+     * Getter to return the name of the table
+     * @return the name of the table used in the instantiation of the object
+     */
     public String getTableName() {return this.tableName;}
+
+    /**
+     * Queries the database to grab the current column names if there is need to update the columns
+     * @throws SQLException - if the handler cannot access the table
+     */
     public void updateColumnNames() throws SQLException {this.columnNames = this.handler.getTableColumnNames(this.tableName);}
 
     /**
-     * 
-     * @param tableName
-     * @param isTest
-     * @throws SQLException
-     * @throws ClassNotFoundException
-     * @throws DataBaseNotFoundException
+     * This constructors works exactly as the original constructor, but SHOULD NOT be used outside of unit testing
+     * @param tableName - name of the table
+     * @param isTest - flag for indicating if this is a test or not. will throw a runtime exception if this flag is not true
+     * @throws SQLException - thrown during datahanlding initilaization 
+     * @throws ClassNotFoundException - thrown during datahanlding initalization
+     * @throws DataBaseNotFoundException - thrown during datahanlding initalization
      */
     public Table(final String tableName, final boolean isTest) throws SQLException, ClassNotFoundException, DataBaseNotFoundException {
         if(!isTest) {throw new RuntimeException("Error, flag for test is not marked 'true'. Please ensure that isTest is true, and that it is being used only in a unit test setting.");}
--- src/test/java/datahandling/TableTest.java
+++ src/test/java/datahandling/TableTest.java
@@ -13,9 +13,6 @@ import com.echonet.datahandling.Table;
 import com.echonet.exceptions.DataBaseNotFoundException;
 
 public class TableTest {
-
-    String testDB = "jdbc:sqlite:src/test/echodatatest.db";
-    String testDBPath = "src/test/echodatatest.db";
     private Table table;
     private final String tableName = "\"TestTable1\"";
     private final List<String> mockColumnNames = Arrays.asList("user_id", "test_col_1", "test_col_2");
@@ -36,17 +33,4 @@ public class TableTest {
         assertNotNull(columns);
         assertEquals(mockColumnNames, columns);
     }
-
-    // @Test
-    // public void testUpdateColumnNames() throws SQLException {
-    //     // Update the mock to return a different set of column names
-    //     List<String> newColumnNames = Arrays.asList("id", "username", "created_at");
-    //     when(mockHandler.getTableColumnNames(tableName)).thenReturn(newColumnNames);
-
-    //     // Call the update method
-    //     table.updateColumnNames();
-
-    //     // Verify that the column names were updated
-    //     assertEquals(newColumnNames, table.getTableColumns());
-    // }
 }
