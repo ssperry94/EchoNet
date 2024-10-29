7d66c4c948a97af05ab997ea0d7de4b15d85ce7c
Calebjb927 <cjb0056@uah.edu>
2024-10-28 21:53:59 -0500

    added initial friends functionality

--- src/main/java/com/echonet/profile/profile.java
+++ src/main/java/com/echonet/profile/profile.java
@@ -1,10 +1,14 @@
 package com.echonet.profile;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class profile {
     private String name;
     private int age;
     private String birthday;
     private String email;
+    private Set<profile> friends;
 
     public void Profile(String name, int age, String birthday, String email) {
         this.name = name;
@@ -67,5 +71,36 @@ public class profile {
         else{
             System.out.println("Missing field: email"); // Placeholder
         }
+    }
+
+    // Friend manegement
+    public Set<profile> getFriends() {
+        return friends;
+    }
+
+    public boolean addFriend(profile friend){
+        if(friend == null){             // checks if friend exists
+            return false;
+        }
+        if(friend == this){             // checks that friend is not yourself
+            return false;
+        }
+        if(friends.contains(friend)){   // check that you are not already friends
+            return false;
+        }
+
+        friends.add(friend);            // adds requested profile to your freinds list
+        friend.friends.add(this);       // adds you to profiles freinds list
+        return true;
+    }   
+
+    public boolean removeFriend(profile friend){
+        if(friend == null || !friends.contains(friend)){
+            return false;
+        }
+
+        friends.remove(friend);         // removes friend from friend lsit
+        friend.friends.remove(this);    // removes you from friends list
+        return true;
     }
 }
