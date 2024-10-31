5f53cb8285be0fc26cb9a7e83230d27db96a6be7
Sam Perry <s.sperry94@gmail.com>
2024-10-31 10:29:28 -0500

    BRANCH:User..........made User extend Domain, and delete table field for User.

--- src/main/java/com/echonet/domainmodel/User.java
+++ src/main/java/com/echonet/domainmodel/User.java
@@ -3,9 +3,8 @@ package com.echonet.domainmodel;
 import java.sql.ResultSet;
 
 import com.echonet.datahandling.Table;
-public class User {
+public class User extends Domain {
 
-    protected Table userTable;
     protected String firstName;
     protected String lastName;
     protected String username;
