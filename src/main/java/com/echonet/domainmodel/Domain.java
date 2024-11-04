8ed6350b675f464fc52838b5f02aa056eb1d3495
Sam Perry <s.sperry94@gmail.com>
2024-11-03 21:09:40 -0600

    BRANCH:MapMethodToDomain.........added abstract method for creating the map to domain, and made Domain an abstract class.

--- src/main/java/com/echonet/domainmodel/Domain.java
+++ src/main/java/com/echonet/domainmodel/Domain.java
@@ -1,10 +1,12 @@
 package com.echonet.domainmodel;
 
+import java.util.Map;
+
 import com.echonet.datahandling.Table;
 /**
  * This class is used as a parent for all classes that will acess the database
  */
-public class Domain {
+abstract class Domain {
     //protected fields
     protected Table table;
 
@@ -13,5 +15,5 @@ public class Domain {
     public Table getTable() {return this.table;}
     public void setTable(Table table) {this.table = table;}
 
-
+    abstract Map <Integer, Object> createMapForBackEnd();
 }
