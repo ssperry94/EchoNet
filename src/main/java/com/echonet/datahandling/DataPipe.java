46e7484a9fcb8f8492c47337497593b8b14ab718
Sam Perry <s.sperry94@gmail.com>
2024-11-04 10:16:03 -0600

    BRANCH:DataPipe.........added basic java docs for read and write methods.

--- src/main/java/com/echonet/datahandling/DataPipe.java
+++ src/main/java/com/echonet/datahandling/DataPipe.java
@@ -11,6 +11,11 @@ public class DataPipe {
     
     public DataPipe() {};
 
+    /**
+     * Method used by client code to read information from the database. must pass a subclass of Domain
+     * @param d - a subclass of Domain that must have a table and ID field
+     * @return - ResultSet containg all of the data in the table associated with the class passed in, null if any exceptions were thrown
+     */
     public ResultSet read(final Domain d) {
         ResultSet rs;
         try {
@@ -23,6 +28,11 @@ public class DataPipe {
         }
     }   
 
+    /**
+     * Method used by clinet code to write to database. Must pass in a subclass of Domain
+     * @param d - a subclass of Domian. must have a table, and an overriden createMapForBackEnd() function
+     * @return - true if data was successfully written, false if any exceptions were thrown
+     */
     public boolean write(final Domain d) {
         Map <Integer, Object> dataMap;
 
@@ -48,7 +58,8 @@ public class DataPipe {
             return null;
         }
     }   
-
+    
+    //for unit test 
     public boolean write(final Domain d, boolean isTest) {
         Map <Integer, Object> dataMap;
 
