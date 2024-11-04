7e4e54c94eaba22b4507f96858d31f46c5dca8f9
Sam Perry <s.sperry94@gmail.com>
2024-11-03 22:38:48 -0600

    BRANCH:DataPipe..........created first iteration of DataPipe.read().

--- src/main/java/com/echonet/datahandling/DataPipe.java
+++ src/main/java/com/echonet/datahandling/DataPipe.java
@@ -1,17 +1,24 @@
 package com.echonet.datahandling;
 
 import java.sql.ResultSet;
-import java.util.Map;
+import java.sql.SQLException;
 
 import com.echonet.domainmodel.Domain;
+import com.echonet.exceptions.DataBaseNotFoundException;
+import com.echonet.utilities.Config;
 public class DataPipe {
     
     public DataPipe() {};
 
     public ResultSet read(final Domain d) {
         ResultSet rs;
-        
-
-        return rs;
-    }
+        try {
+            DataReader reader = new DataReader(Config.DATABASE_DRIVER);
+            rs = reader.read(d.getTable(), d);
+            return rs;
+        } catch (SQLException | ClassNotFoundException | DataBaseNotFoundException e) {
+            //throw error message
+            return null;
+        }
+    }   
 }
