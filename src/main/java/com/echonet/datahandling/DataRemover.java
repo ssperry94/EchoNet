a681240bde4785d748d0f8b1e3d400e86c0b3b86
Sam Perry <s.sperry94@gmail.com>
2024-11-06 16:08:14 -0600

    BRANCH:DataRemover.......created the first iteration of the remove function.

--- src/main/java/com/echonet/datahandling/DataRemover.java
+++ src/main/java/com/echonet/datahandling/DataRemover.java
@@ -1,6 +1,7 @@
 package com.echonet.datahandling;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.echonet.domainmodel.Domain;
 import com.echonet.exceptions.DataBaseNotFoundException;
@@ -11,7 +12,11 @@ public class DataRemover extends DataHandler {
         super(database);
     }
 
-    public void remove(final Table t, final Domain d) {
+    public void remove(final Table t, final Domain d) throws SQLException {
+        String sql = sqlgen.deleteStatement(t, d);
+        Statement stmt = c.createStatement();
+        stmt.executeUpdate(sql);
+        stmt.close();
         
     }
 }
