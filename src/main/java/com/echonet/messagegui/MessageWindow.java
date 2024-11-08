1ef141621cd30bec93a5c4b0777048860b174641
Sam Perry <s.sperry94@gmail.com>
2024-11-08 12:13:01 -0600

    BRANCH:MessageGUI.........added button display panel and button, no functionality behind buttons yet.
    next: find out how to have more than 1 jpanel per jframe.

--- src/main/java/com/echonet/messagegui/MessageWindow.java
+++ src/main/java/com/echonet/messagegui/MessageWindow.java
@@ -1,23 +1,44 @@
 package com.echonet.messagegui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MessageWindow {
     private JFrame mainWindow;
+    private JPanel buttonPanel;
+    private JPanel displayPanel;
     private JButton sendMessage;
     private JButton updateMessage;
     
     private void initalize() {
-        mainWindow = new JFrame();
-        mainWindow.setTitle("Messages");
-        mainWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        mainWindow.setSize(800, 500);
-        mainWindow.setLocationRelativeTo(null);
+        this.mainWindow = new JFrame();
+        this.mainWindow.setTitle("Messages");
+        this.mainWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.mainWindow.setLayout(new BorderLayout(20,10));
+        this.mainWindow.setSize(800, 500);
+        this.mainWindow.setLocationRelativeTo(null);
+
+        this.displayPanel = new JPanel();
+        this.displayPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        this.displayPanel.setBackground(Color.BLUE);
+        this.buttonPanel = new JPanel();
+        this.buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 200, 10));
+        this.mainWindow.add(buttonPanel, BorderLayout.SOUTH);
+
+        buttonPanel.setBackground(Color.GRAY);
+        this.initalizeButtons();
     }
 
     private void initalizeButtons() {
-        
+        this.sendMessage = new JButton("Send Message");
+        this.updateMessage = new JButton("Update Messages");
+        this.buttonPanel.add(sendMessage);
+        this.buttonPanel.add(updateMessage);
     }
     public MessageWindow() {
         this.initalize();
