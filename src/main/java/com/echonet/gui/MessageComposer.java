6799e3959473372cf99a8b80d5c39df4a9e653ad
Sidney Howard III <sidneyhowardiv@gmail.com>
2024-11-21 18:45:49 -0600

    newest gui changes

--- src/main/java/com/echonet/gui/MessageComposer.java
+++ src/main/java/com/echonet/gui/MessageComposer.java
@@ -6,8 +6,12 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Map;
-
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -32,122 +36,15 @@ public class MessageComposer {
     private JPanel buttonPanel;
     private JEditorPane messageBox;
     private JButton sendButton;
-    private JEditorPane recipiantBox;
-    private JLabel recipantLabel;
+    private JButton updateButton; // Added Update Messages button
+    private JEditorPane recipientBox;
+    private JLabel recipientLabel;
     private JLabel contentsLabel;
     private JScrollPane contentsScrollBar;
 
-    /*TODO: change recipiant box to a drop box that will look at a user's friends first
-     */
-    private void sendMessage() { //change when user class has table field set in all constructors
-        DataPipe dataPipe = new DataPipe();
-        User u;
-        Message message;
-        //check to make sure recipiant exists
-        String recipiantUsername = this.recipiantBox.getText();
-
-        //if nothing was entered
-        if(recipiantUsername.equals("")) {
-            JOptionPane.showMessageDialog(mainWindow, "Error, must have recipiant.", "RECIPIANTERROR", JOptionPane.ERROR_MESSAGE);
-            this.messageBox.setText("");
-            return;
-        }
-        try {
-            u = new User(1); //will be able to change when constructor is fixed
-            u.setTable(new Table(Config.USER_TABLE));
-    
-            Map <String, Object> userMap = dataPipe.read(u, "username", recipiantUsername);
-            if(userMap == null) {
-                JOptionPane.showMessageDialog(mainWindow, "Cannot find given user. Please make sure that you entered the correct username.", "RECIPIANTNOTFOUND", JOptionPane.ERROR_MESSAGE);
-                this.messageBox.setText("");
-                return;
-            }
-            String actualUsername = (String) userMap.get("username");
-            if(!actualUsername.equals(recipiantUsername)) {
-                JOptionPane.showMessageDialog(mainWindow, "Internal error occured. Message failed to send", "USERNAMEMISMATCH", JOptionPane.ERROR_MESSAGE);
-                this.messageBox.setText("");
-                return;
-            }
-            message = new Message((int) userMap.get("userID"));
-
-            message.setContents(this.messageBox.getText());
-            message.setAutomaticMessageID();
-            message.setTimeStamp();
-            message.setRecipiantID(this.currentUser.getID());
-
-            dataPipe.write(message);
-            this.messageBox.setText("");
-            JOptionPane.showMessageDialog(mainWindow, "Message Sent!", "MESSAGESUCCESS", JOptionPane.INFORMATION_MESSAGE);
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(mainWindow, "Unknown Error occured. Message failed to send", "UNKOWNERROR", JOptionPane.ERROR_MESSAGE);
-            e.printStackTrace();
-        }
-    }
-
-    private void initalizeEditors() {
-        this.recipantLabel = new JLabel();
-        this.recipantLabel.setText("To:");
-
-        this.recipiantBox = new JEditorPane();
-        this.recipiantBox.setMaximumSize(new Dimension(300, 20));
-        this.recipiantBox.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
-        this.contentsLabel = new JLabel();
-        this.contentsLabel.setText("Contents:");
-
-        this.messageBox = new JEditorPane();
-        this.messageBox.setPreferredSize(new Dimension(500,500));
-        this.messageBox.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        this.contentsScrollBar = new JScrollPane(this.messageBox);
-
-        this.composerPanel.add(this.recipantLabel);
-        this.composerPanel.add(this.recipiantBox);
-        this.composerPanel.add(this.contentsLabel);
-        this.composerPanel.add(this.contentsScrollBar);
-    }
-
-    private void initalizeButtons() {
-        this.sendButton = new JButton();
-        this.sendButton.setText("Send");
-        this.sendButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-               sendMessage();
-            }
-        });
-
-        this.buttonPanel.add(this.sendButton);
-    }
-
-    private void initalizePanels() {
-        this.composerPanel = new JPanel();
-        this.composerPanel.setLayout(new BoxLayout(composerPanel, BoxLayout.Y_AXIS));
-
-        this.buttonPanel = new JPanel();
-        this.buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
-    }
-
-    private void initalize() {
-        this.mainWindow = new JFrame();
-        this.mainWindow.setTitle("Message Composer");
-        this.mainWindow.setLayout(new BorderLayout(10, 10));
-        this.mainWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.mainWindow.setSize(500,500);
-
-        //initalize message box
-        this.initalizePanels();
-        //initalize buttons
-        this.initalizeButtons();
-        //initalize recipant box
-        this.initalizeEditors();
-
-        this.mainWindow.add(this.buttonPanel, BorderLayout.SOUTH);
-        this.mainWindow.add(this.composerPanel, BorderLayout.CENTER);
-    }
-
     public MessageComposer(final User currentUser) {
         this.currentUser = currentUser;
-        this.initalize();
+        this.initialize();
     }
 
     public void show() {
@@ -156,5 +53,94 @@ public class MessageComposer {
 
     public JFrame getMainWindow() {
         return this.mainWindow;
+    }
+
+    private void sendMessage() {
+        // Placeholder logic for sending messages
+        System.out.println("Message Sent!");
+        JOptionPane.showMessageDialog(mainWindow, "Message Sent!", "Message", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void updateMessages() {
+        // Placeholder logic for updating messages
+        System.out.println("Messages Updated!");
+
+        // Play sound effect when messages are updated
+        playSound("echo.wav");
+
+        JOptionPane.showMessageDialog(mainWindow, "Messages Updated!", "Update", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void playSound(String soundFileName) {
+        try {
+            File soundFile = new File(soundFileName); // Ensure the file path is correct
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(soundFile));
+            clip.start(); // Play the sound
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeEditors() {
+        this.recipientLabel = new JLabel("To:");
+        this.recipientBox = new JEditorPane();
+        this.recipientBox.setMaximumSize(new Dimension(300, 20));
+        this.recipientBox.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        this.contentsLabel = new JLabel("Contents:");
+        this.messageBox = new JEditorPane();
+        this.messageBox.setPreferredSize(new Dimension(500, 500));
+        this.messageBox.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        this.contentsScrollBar = new JScrollPane(this.messageBox);
+
+        this.composerPanel.add(this.recipientLabel);
+        this.composerPanel.add(this.recipientBox);
+        this.composerPanel.add(this.contentsLabel);
+        this.composerPanel.add(this.contentsScrollBar);
+    }
+
+    private void initializeButtons() {
+        this.sendButton = new JButton("Send");
+        this.sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendMessage();
+            }
+        });
+
+        this.updateButton = new JButton("Update Messages"); // Update Messages button
+        this.updateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateMessages(); // Play sound and update messages
+            }
+        });
+
+        this.buttonPanel.add(this.sendButton);
+        this.buttonPanel.add(this.updateButton); // Add Update Messages button
+    }
+
+    private void initializePanels() {
+        this.composerPanel = new JPanel();
+        this.composerPanel.setLayout(new BoxLayout(composerPanel, BoxLayout.Y_AXIS));
+
+        this.buttonPanel = new JPanel();
+        this.buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
+    }
+
+    private void initialize() {
+        this.mainWindow = new JFrame();
+        this.mainWindow.setTitle("Message Composer");
+        this.mainWindow.setLayout(new BorderLayout(10, 10));
+        this.mainWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.mainWindow.setSize(500, 500);
+
+        this.initializePanels();
+        this.initializeButtons();
+        this.initializeEditors();
+
+        this.mainWindow.add(this.buttonPanel, BorderLayout.SOUTH);
+        this.mainWindow.add(this.composerPanel, BorderLayout.CENTER);
     }
 }
--- src/main/java/com/echonet/gui/MessageWindow.java
+++ src/main/java/com/echonet/gui/MessageWindow.java
@@ -3,10 +3,6 @@ package com.echonet.gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -14,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 
@@ -25,31 +20,43 @@ import com.echonet.domainmodel.User;
 import com.echonet.exceptions.DataBaseNotFoundException;
 import com.echonet.utilities.Config;
 
-/* TODO: add borders, colors, etc to various parts of messages 
-*/
+public class MessageWindow extends JPanel {
 
-public class MessageWindow {
     private User currentUser;
-    private MessageComposer composer;
-    private JFrame mainWindow;
+    private MainFrame mainFrame;
     private JPanel buttonPanel;
     private JPanel displayPanel;
     private JButton sendMessage;
     private JButton updateMessage;
-    private List <JTextPane> messageDisplay;
+    private JButton backButton; // Back button
+    private List<JTextPane> messageDisplay;
+
+    public MessageWindow(final User currentUser, MainFrame mainFrame) throws Exception {
+        this.currentUser = currentUser;
+        this.mainFrame = mainFrame; // Store MainFrame for back navigation
+        this.messageDisplay = new ArrayList<>();
+
+        this.setLayout(new BorderLayout(20, 10));
+
+        initializeButtons();
+        initializeButtonsPanel();
+        initializeMessagePanel();
+
+        this.add(buttonPanel, BorderLayout.SOUTH);
+        this.add(displayPanel, BorderLayout.CENTER);
+    }
 
     private User getRecipiant(final Message m) {
         DataPipe dataPipe;
         User recipiant;
-        Map <String, Object> userMap;
-        String firstName, lastName;
+        Map<String, Object> userMap;
         try {
             dataPipe = new DataPipe();
             recipiant = new User(m.getRecipiantID());
             recipiant.setTable(new Table(Config.USER_TABLE));
             userMap = dataPipe.read(recipiant);
 
-            if(userMap == null) {
+            if (userMap == null) {
                 recipiant.setFirstName("Unknown");
                 recipiant.setLastName("User");
                 return recipiant;
@@ -63,19 +70,21 @@ public class MessageWindow {
             e.printStackTrace();
             return null;
         }
-
     }
+
     private JTextPane createMessageBox(final Message message) {
         User recipiant = this.getRecipiant(message);
-        String messageBoxText = "From: " + recipiant.getFirstName() + " " + recipiant.getLastName() + "\nSent: " + message.getTimeStampString() + "\nContents: " + message.getContents();
+        String messageBoxText = "From: " + recipiant.getFirstName() + " " + recipiant.getLastName()
+                + "\nSent: " + message.getTimeStampString() + "\nContents: " + message.getContents();
         JTextPane messageBox = new JTextPane();
         messageBox.setEditable(false);
         messageBox.setText(messageBoxText);
         return messageBox;
-        }
-    private Message createMessage(final Map <String, Object> dataMap) throws SQLException, DataBaseNotFoundException, ClassNotFoundException {
-        //all fields of a message class
-        Message message; 
+    }
+
+    private Message createMessage(final Map<String, Object> dataMap)
+            throws SQLException, DataBaseNotFoundException, ClassNotFoundException {
+        Message message;
         int primaryID, messageID, recipiantID;
         String contents;
         Timestamp timestamp;
@@ -90,120 +99,74 @@ public class MessageWindow {
     }
 
     private void updateMessages() {
-        List <Map <String, Object>> messageHistory; //holds all messages gathered from database
+        List<Map<String, Object>> messageHistory;
         JTextPane messageBox;
         try {
             Message dummymsg = new Message(this.currentUser.getID());
             DataPipe dataPipe = new DataPipe();
             messageHistory = dataPipe.multiRead(dummymsg);
 
-            if(messageHistory.isEmpty()) {
+            if (messageHistory.isEmpty()) {
                 dummymsg.setContents("No messages.....yet!");
                 messageBox = this.createMessageBox(dummymsg);
                 this.messageDisplay.add(messageBox);
-            }
-            else {
-                for (Map <String, Object> dataMap : messageHistory) {
+            } else {
+                for (Map<String, Object> dataMap : messageHistory) {
                     dummymsg = this.createMessage(dataMap);
                     messageBox = this.createMessageBox(dummymsg);
                     messageDisplay.add(messageBox);
-                    }
+                }
             }
 
         } catch (SQLException | ClassNotFoundException | DataBaseNotFoundException e) {
             e.printStackTrace();
             System.exit(1);
-            //throw error box
         }
     }
-    private void initalizeButtonsPanel() {
+
+    private void initializeButtons() {
+        this.sendMessage = new JButton("Send Message");
+        this.sendMessage.addActionListener(e -> {
+            // Open the MessageComposer when clicked
+            MessageComposer composer = new MessageComposer(currentUser);
+            composer.show();
+        });
+
+        this.updateMessage = new JButton("Update Messages");
+        this.updateMessage.addActionListener(e -> {
+            displayPanel.removeAll();
+            messageDisplay.clear();
+            updateMessages();
+            for (JTextPane messageBox : messageDisplay) {
+                displayPanel.add(messageBox);
+            }
+            displayPanel.updateUI();
+        });
+
+        this.backButton = new JButton("Back to Home");
+        this.backButton.addActionListener(e -> {
+            // Navigate back to HomePanel
+            mainFrame.showPanel("HomePanel");
+        });
+    }
+
+    private void initializeButtonsPanel() {
         this.buttonPanel = new JPanel();
-        this.buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 200, 10));
-        buttonPanel.setBackground(Color.GRAY);
+        this.buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 10));
+        this.buttonPanel.setBackground(Color.GRAY);
+        this.buttonPanel.add(backButton); // Add back button
         this.buttonPanel.add(sendMessage);
         this.buttonPanel.add(updateMessage);
     }
 
-    private void initalizeMessagePanel() throws Exception {
+    private void initializeMessagePanel() {
         this.displayPanel = new JPanel();
         this.displayPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
-        this.displayPanel.setBackground(Color.BLUE);
-        this.updateMessages();
+        this.displayPanel.setBackground(new Color(146, 199, 195));
+        updateMessages();
 
-        for(JTextPane messageBox : this.messageDisplay) {
+        for (JTextPane messageBox : this.messageDisplay) {
             this.displayPanel.add(messageBox);
         }
     }
-
-    private void initalize() throws Exception {
-        this.messageDisplay = new ArrayList<>();
-        this.mainWindow = new JFrame();
-        this.mainWindow.setTitle("Messages");
-        this.mainWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.mainWindow.setLayout(new BorderLayout(20,10));
-        this.mainWindow.setSize(800, 500);
-        this.mainWindow.setLocationRelativeTo(null);
-
-        this.initalizeButtons();
-        this.initalizeButtonsPanel();
-        this.initalizeMessagePanel();
-
-        this.mainWindow.addWindowListener(new WindowAdapter() {
-        @Override
-        public void windowClosed(WindowEvent e) {
-            if(composer.getMainWindow() != null) {
-                composer.getMainWindow().dispose();
-            }
-        }
-        });
-
-        this.mainWindow.add(buttonPanel, BorderLayout.SOUTH);
-        this.mainWindow.add(displayPanel, BorderLayout.CENTER);
-
-    }
-
-    private void initalizeButtons() {
-        this.sendMessage = new JButton("Send Message");
-        this.sendMessage.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                composer = new MessageComposer(currentUser);
-                composer.show();
-            }
-        });
-        this.updateMessage = new JButton("Update Messages");
-        this.updateMessage.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displayPanel.removeAll();
-                messageDisplay.clear();
-                updateMessages();
-                for(JTextPane messageBox : messageDisplay) {
-                    displayPanel.add(messageBox);
-                }
-                displayPanel.updateUI();
-            }});
-    }
-
-    public MessageWindow(final User currentUser) throws Exception{
-        this.currentUser = currentUser;
-        this.initalize();
-    }
-
-    public void show() {
-        mainWindow.setVisible(true);
-    }
-
-    //for running window
-    public static void main(String args[]) {
-    try {
-        User testUser = new User(1);
-        MessageWindow window = new MessageWindow(testUser);
-        window.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
-
