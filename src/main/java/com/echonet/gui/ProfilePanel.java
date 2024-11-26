1e49ea72176c90a630c10b3b48f2d95b4d79c63c
Sam Perry <s.sperry94@gmail.com>
2024-11-26 11:37:59 -0600

    BRANCH:ProfileViewer..........added jlabels and buttons.

--- src/main/java/com/echonet/gui/ProfilePanel.java
+++ src/main/java/com/echonet/gui/ProfilePanel.java
@@ -1,5 +1,9 @@
 package com.echonet.gui;
 
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.echonet.domainmodel.User;
@@ -8,16 +12,28 @@ public class ProfilePanel extends JPanel {
     private MainFrame mainWindow;
     private User currentUser;
 
+    private JLabel titleLabel;
+    private JLabel nameLabel, usernameLabel, birthdayLabel, emailLabel; //labels displaying what each field is 
+    private JLabel nameDisplayLabel, usernameDisplayLabel, birthdayDisplayLabel, emailDisplayLabel; //labels containing actual information of each field
+    
+    private JButton backButton;
+
     private void initalize() {
+        this.setLayout(new GridBagLayout());
+
+        //initalize labels
+
 
     }
+
     public ProfilePanel(final MainFrame mw, final User u) {
         this.mainWindow = mw;
         this.currentUser = u;
         this.initalize();
     }
 
-    public void main(String args[]) {
-        this.setVisible(true);
+    public static void main(String args[]) {
+        ProfilePanel p = new ProfilePanel(new MainFrame(), new User(1));
+        p.setVisible(true);
     }
 }
