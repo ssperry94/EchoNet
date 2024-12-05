2ddbdaad7f15c9bb630c21c8234556d8aff26467
Calebjb927 <cjb0056@uah.edu>
2024-12-04 18:32:08 -0600

    Adding Java Docs

--- src/main/java/com/echonet/domainmodel/Friend.java
+++ src/main/java/com/echonet/domainmodel/Friend.java
@@ -1,23 +1,56 @@
 package com.echonet.domainmodel;
 
+/**
+ * Represents a friendship between two users in the system.
+ * This class extends {@code User} to encapsulate a relationship between two users.
+ * The friendship is bidirectional, meaning the order of users does not matter.
+ * @Author Caleb Blair
+ */
 public class Friend extends User{
     private final User user1;
     private final User user2;
-
+    /**
+     * Constructs a {@code Friend} object that represents a relationship between two users.
+     * 
+     * @param user1 the first user in the friendship
+     * @param user2 the second user in the friendship
+     * @author Caleb Blair
+     */
     public Friend(User user1, User user2) {
         super(user1.getID());
         this.user1 = user1;
         this.user2 = user2;
     }
 
+    /**
+     * Returns the first user in the friendship.
+     * 
+     * @return the first user
+     * @author Caleb Blair
+     */
     public User getUser1() {
         return user1;
     }
 
+    /**
+     * Returns the second user in the friendship.
+     * 
+     * @return the second user
+     * @author Caleb Blair
+     */
     public User getUser2() {
         return user2;
     }
 
+    /**
+     * Checks if this {@code Friend} object is equal to another object.
+     * Two {@code Friend} objects are considered equal if they involve the same users,
+     * regardless of the order of the users.
+     * 
+     * @param obj the object to compare
+     * @return {@code true} if the objects are equal, {@code false} otherwise
+     * @author Caleb Blair
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
